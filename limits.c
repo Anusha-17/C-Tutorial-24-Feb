88c6a5db8220425ef4240d76eb3efc3a3f885042
Anusha-17 <98573933+Anusha-17@users.noreply.github.com>
2022-02-24 22:09:20 +0530

    Limits

---
+++ limits.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<limits.h>
+int main()
+{
+	printf("Signed short min value: %d\n",SHRT_MIN);
+	printf("Signed short max value: %d\n",SHRT_MAX);
+    printf("Unsigned short max value: %d\n",USHRT_MAX);
+    printf("Signed int min value: %d\n",INT_MIN);
+    printf("Signed int max value: %d\n",INT_MAX);
+}
