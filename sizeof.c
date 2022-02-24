b796e82159e505ecdeceafdf7ba963dc09943146
Anusha-17 <98573933+Anusha-17@users.noreply.github.com>
2022-02-24 22:10:43 +0530

    Sizeof  operator

---
+++ sizeof.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	char c;
+	short s;
+	printf("Size of char: %d\n",sizeof(c));
+	printf("Size of short: %d\n",sizeof(s));
+	printf("Size of float: %d\n",sizeof(float));
+}
